916433d609382c8834a0c7a73f8eecb10d280479
Michael Barrows <mail@michaelbarrows.com>
2021-12-28 16:59:19 +0000

    languages data

--- original-data/languages.py
+++ original-data/languages.py
@@ -1,34 +1,36 @@
-import dataset
 import helpers
+import dataset
+import numpy
+import pandas as pd
+import language_code_mapper
 
-def number_of_languages_in_dataset():
-    langs = []
-
-    for file in dataset.files:
-        df = helpers.load_dataset(file)
-        print(file)
+def get_language_counts():
+    langs = {}
+    total = 0
+    for ds in dataset.files:
+        print(ds)
+        df = helpers.load_dataset(ds)
         for index, row in df.iterrows():
+            total += 1
             if row['tweet_language'] not in langs:
-                langs.append(row['tweet_language'])
+                langs[row['tweet_language']] = 1
+            else:
+                langs[row['tweet_language']] += 1
 
-        print(str(len(langs)))
-    
-    print('Total languages in all dataset files ' + str(len(langs)))
-    store_result(str(len(langs)))
-    
-def store_result(total):
-    helpers.path_checker(dataset.output_data_store)
-    output_file = 'total_languages_in_all_datasets.csv'
+    langs = helpers.sort_dict(langs)
+    data = list(langs.items())
+    langs_array = numpy.array(data)
 
-    helpers.data_to_file_two_values(
-        [
-            [
-                'total languages', 
-                str(total)
-            ]
-        ],
-        dataset.output_data_store + '/' + output_file
-    )
+    new_langs = []
+    for lang in langs_array:
+        if lang[0] in language_code_mapper.iso:
+            new_langs.append([language_code_mapper.iso[lang[0]] + " (" + lang[0] + ")", lang[1], round((int(lang[1]) / total) * 100, 2)])
+        else:
+            new_langs.append([lang[0], lang[1], round((int(lang[1]) / total) * 100, 2)])
 
+    new_langs = pd.DataFrame(new_langs, columns=['language', 'volume', 'percentage'])
+    print(new_langs)
 
-number_of_languages_in_dataset()
+    helpers.dataframe_to_csv(new_langs, dataset.output_data_store + '/language_counts.csv')
+
+get_language_counts()
