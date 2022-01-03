685877ddd2f1ca163deae4dbe2f0ce3848043d12
Michael Barrows <mail@michaelbarrows.com>
2022-01-03 12:44:56 +0000

    normalise reported countries and count those

--- processed-data/user_reported_countries.py
+++ processed-data/user_reported_countries.py
@@ -5,24 +5,16 @@ import pandas
 def count_unique_values(df):
     counter = {}
     processed_users = []
-    mapper = {}
-    mapper['nan'] = "Missing"
     for index, row in df.iterrows():
         if row.userid not in processed_users:
             processed_users.append(row.userid)
-            location = str(row.user_reported_location).lower()
+            location = row.location
 
-            if location not in mapper:
-                response = input(location + " : ")
-                mapper[location] = response
+            if location not in counter:
+                counter[location] = 1
             else:
-                response = mapper[location]
-                
-            if response not in counter:
-                counter[response] = 1
-            else:
-                counter[response] += 1
-
+                counter[location] += 1
+            
     counter = helpers.sort_dict(counter)
     data = []
     for row in counter:
@@ -31,8 +23,29 @@ def count_unique_values(df):
     new_df = pandas.DataFrame(data, columns=['location', 'users'])
     helpers.dataframe_to_csv(new_df, 'output_data/user_reported_location_volumes.csv')
 
+def normalise_labels(df):
+    df['location'] = ""
+    mapper = {}
+    mapper['nan'] = "Missing"
+    for index, row in df.iterrows():
+        location = str(row.user_reported_location).lower()
+
+        if location not in mapper:
+            response = input(location + " : ")
+            mapper[location] = response
+        else:
+            response = mapper[location]
+            
+        df.location.at[index] = response
+
+    helpers.dataframe_to_csv(df, 'output_data/location_labelled_filtered_dataset.csv')
+    return df
+    
+
 def import_dataset():
     df = helpers.load_dataset(dataset.filtered_dataset)
     return df
 
-count_unique_values(import_dataset())
+df = import_dataset()
+df = normalise_labels(df)
+count_unique_values(df)
