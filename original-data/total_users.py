bc97511cb823b83222020fed0d1b08989d9c67a9
Michael Barrows <mail@michaelbarrows.com>
2022-04-16 11:36:35 +0100

    get total users from original datasets

---
+++ original-data/total_users.py
@@ -0,0 +1,29 @@
+import helpers
+import dataset
+import pandas
+
+def import_dataset(ds):
+    return helpers.load_dataset(ds)
+
+def process():
+    store = {}
+
+    for ds in dataset.files:
+        print(ds)
+        df = ''
+        df = import_dataset(ds)
+        for index, row in df.iterrows():
+            if row.userid in store:
+                store[row.userid] += 1
+            else:
+                store[row.userid] = 1 
+                print('new user ' + str(len(store)))
+                
+        print("Complete - " + ds)
+        print(str(len(store)))
+    return store
+
+
+data = process()
+
+print('Total Users Across All Files: ' + str(len(data)))
